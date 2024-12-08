1373a03127d10afcd686e0cb033cb1ffcb07d887
Edo404 <edoardogamurrini@gmail.com>
2024-12-08 11:13:53 +0100

    prova add

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ let ToDoList = [];
 
 const addBtn = document.querySelector('.add');
 const listItems = document.querySelector('.list');
+const item = document.querySelector('li');
+
 
 addBtn.addEventListener("click", function(e) {
     const actualText = document.getElementById('text').value;
     ToDoList.push(actualText);
     console.log(ToDoList);
+    listItems.textContent = ToDoList;
 })
