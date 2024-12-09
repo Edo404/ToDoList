4a3d432e99747444ade8306a7064432add931b64
Edo404 <edoardogamurrini@gmail.com>
2024-12-09 17:30:02 +0100

    remove ok

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ let ul = document.querySelector('ul');
 //when click the add btn, get the value of the text inserted
 //create a li HTML
 //assign to the textContent of the li, the text inserted
+//create the edit and the trash icons inside the li
 //append the li element in the ul list
 addBtn.addEventListener("click", function() {
     let actualText = textArea.value;
-    let li = document.createElement('li');
-    let edit = document.createElement('i');
-    let trash = document.createElement('i');
-    li.textContent = actualText;
+    let row = document.createElement('li');
+    row.textContent = actualText;
+
+    let edit = document.createElement('i')
+    let trash = document.createElement('i')
+
     edit.className = "fa-solid fa-pen";
     trash.className = "fa-solid fa-trash";
+    row.appendChild(edit);
+    row.appendChild(trash);
+    ul.appendChild(row);
 
-    ul.appendChild(li);
-    ul.appendChild(edit);
-    ul.appendChild(trash);
-})
-
-let trashIcon = document.querySelector('.fa-solid fa-trash');
-
-trashIcon.addEventListener("click", function() {
-    console.log("click avvenuto");
+    //remove the element if the trash icon is clicked
+    trash.addEventListener("click", function() {
+        ul.removeChild(row);
+    })
 })
